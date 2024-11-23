0cb8abcaaa0cd80f27d58ea6baed9d64bfc92994
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-11-22 19:35:35 -0500

    navigate with animaion

--- lib/widgets/labels.dart
+++ lib/widgets/labels.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:realtime_chat/pages/login_page.dart';
+import 'package:realtime_chat/pages/register_page.dart';
 
 class Labels extends StatelessWidget {
   final String ruta;
@@ -15,30 +17,60 @@ class Labels extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-      children: [
-        Text(
-          subtitulo,
-          style: const TextStyle(
-              color: Colors.black54, fontSize: 15, fontWeight: FontWeight.w300),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        GestureDetector(
+    return Column(
+    children: [
+      Text(
+        subtitulo,
+        style: const TextStyle(
+            color: Colors.black54, fontSize: 15, fontWeight: FontWeight.w300),
+      ),
+      const SizedBox(
+        height: 10,
+      ),
+      GestureDetector(
           onTap: () {
-            Navigator.pushReplacementNamed(context, ruta);
+            Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) {
+                  // Definir la página a la que quieres navegar
+                  if (ruta == 'login') {
+                    return const LoginPage(); // Reemplaza con tu página de login
+                  } else if (ruta == 'register') {
+                    return const RegisterPage(); // Reemplaza con tu página de registro
+                  }
+                  return Container(); // Página por defecto en caso de no coincidir
+                },
+                transitionsBuilder:
+                    (context, animation, secondaryAnimation, child) {
+                  var begin = ruta == 'login'
+                      ? const Offset(-1.0, 0.0) // Desliza desde la izquierda
+                      : const Offset(1.0, 0.0); // Desliza desde la derecha
+
+                  var end = Offset.zero;
+                  var curve = Curves.easeInOut;
+
+                  var tween = Tween(begin: begin, end: end)
+                      .chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+
+                  return SlideTransition(
+                      position: offsetAnimation, child: child);
+                },
+                transitionDuration: const Duration(milliseconds: 300),
+              ),
+            );
           },
           child: Text(
             titulo,
             style: TextStyle(
-                color: Colors.red[600],
-                fontSize: 18,
-                fontWeight: FontWeight.bold),
+              color: Colors.red[600],
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-      ],
-    ));
+    ],
+        );
   }
 }
