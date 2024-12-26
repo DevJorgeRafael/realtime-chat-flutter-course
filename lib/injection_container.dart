1e1b6b4a704774be0530fb331726f06cc6d6857b
Rafael2408 <rafaelrosero.r.r2408@gmail.com>
2024-12-25 23:46:10 -0500

    refactoring for listeners

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,11 +3,13 @@ import 'package:realtime_chat/apps/auth/domain/auth_service.dart';
 import 'package:realtime_chat/apps/home/domain/chat_service.dart';
 import 'package:realtime_chat/apps/home/domain/socket_service.dart';
 import 'package:realtime_chat/apps/home/domain/users_service.dart';
+import 'package:realtime_chat/config/theme/app_theme.dart';
 import 'package:realtime_chat/shared/service/global_usuario_service.dart';
 
 final GetIt sl = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
+  sl.registerSingleton<AppTheme>(AppTheme());
   // Register the services
   sl.registerLazySingleton<AuthService>(() => AuthService());
   sl.registerLazySingleton<ChatService>(() => ChatService());
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:realtime_chat/app.dart';
 import 'package:realtime_chat/injection_container.dart';
-import 'package:realtime_chat/config/routes/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Forzar la orientación vertical
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
+
   await setupServiceLocator();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      debugShowCheckedModeBanner: false,
-      title: 'Real Time Chat',
-      routerConfig: AppRouter.router,
-    );
-  }
 }
